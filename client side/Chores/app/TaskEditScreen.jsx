c7b9d68e42ecc15d6973d4c4b2956223c54587db
Roni Keinan <ronikeinan17@gmail.com>
2025-03-31 15:18:17 +0300

    up

---
+++ client side/Chores/app/TaskEditScreen.jsx
@@ -0,0 +1,14 @@
+import React, { useEffect, useState } from "react";
+import { useLocalSearchParams, useRouter } from "expo-router";
+import { useTasks } from "./Context/TaskContext";
+
+
+const TaskEditScreen = () => {
+  const router = useRouter();
+
+return (
+  <h1>hi</h1>
+)
+}
+
+export default TaskEditScreen;
--- client side/Chores/app/_layout.jsx
+++ client side/Chores/app/_layout.jsx
@@ -36,6 +36,7 @@ export default function RootLayout() {
                 <Stack.Screen name="EditProfileScreen" />
                 <Stack.Screen name="AddTaskScreen" />
                 <Stack.Screen name="TaskDetailsScreen" />
+                <Stack.Screen name="TaskEditScreen" />
                 <Stack.Screen name="HomePageScreen" />
                 <Stack.Screen name="ChatScreen" />
               </Stack>
