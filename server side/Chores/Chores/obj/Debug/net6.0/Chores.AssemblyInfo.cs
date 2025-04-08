5d24b2e94e7c5133afb892f0e2138f9bc3a7a562
Roni Keinan <ronikeinan17@gmail.com>
2025-04-08 11:43:32 +0300

    up

--- server side/Chores/Chores/obj/Debug/net6.0/Chores.AssemblyInfo.cs
+++ server side/Chores/Chores/obj/Debug/net6.0/Chores.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Chores")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6e0bfbb7ec687e29ff79dcce66ff6004f8de13f0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7f3b69006d9a07e25b076dba457baa51da5af175")]
 [assembly: System.Reflection.AssemblyProductAttribute("Chores")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Chores")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
