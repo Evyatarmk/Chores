7d4d82d6bac0e44596cfa5ba81b81579f42dd90f
Evyatarmk <evyatar.mt@gmail.com>
2025-04-21 20:58:30 +0300

    up

--- server side/Chores/Chores/Controllers/MediaController.cs
+++ server side/Chores/Chores/Controllers/MediaController.cs
@@ -1,6 +1,5 @@
 ﻿using Chores.Data;
 using Chores.Models;
-using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
@@ -18,7 +17,7 @@ namespace Chores.Controllers
             _context = context;
         }
         [HttpGet("home/{homeId}/media")]
-        [Authorize]
+
         public async Task<IActionResult> GetMediaForHome(string homeId)
         {
             var home = await _context.Homes
@@ -56,7 +55,6 @@ namespace Chores.Controllers
 
 
         [HttpPost("home/{homeId}/users/{userId}/media")]
-        [Authorize]
         public async Task<IActionResult> AddMediaItem(string homeId, string userId, [FromForm] CreateMediaItemDto dto)
         {
             // בדיקת בית
@@ -140,7 +138,6 @@ namespace Chores.Controllers
 
 
         [HttpDelete("home/{homeId}/users/{userId}/media/{mediaId}")]
-        [Authorize]
         public async Task<IActionResult> DeleteMediaItem(string homeId, string userId, string mediaId)
         {
             // חיפוש הבית לפי ID
